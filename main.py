697664e27b8eb56c47fe5330e000211db7c6f769
burdukov <brdkv19@gmail.com>
2024-09-09 18:38:57 +0500

    too lazy for commit message :p

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from services.config import settings
 from handlers import inline_images
-from dotenv import load_dotenv
 
-load_dotenv()
 
 async def main():
     bot = Bot(token=settings.bot_token)
     dp = Dispatcher()
     dp.include_routers(inline_images.router)
-    
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
