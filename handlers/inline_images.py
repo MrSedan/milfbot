dbe6a04242e43a8689836402c6a01b61216d9c8b
burdukow <brdkv19@gmail.com>
2024-09-10 10:44:26 +0500

    lol

--- handlers/inline_images.py
+++ handlers/inline_images.py
@@ -9,37 +9,39 @@ router = Router()
 
 @router.inline_query(F.query.contains("pic"))
 async def show_user_links(inline_query: InlineQuery):
-
-    service = inline_query.query.split(" ")[1]
-    tags = inline_query.query.split(f"pic {service}", 1)[-1].strip()
-    result = []
-    if service == "r34":
-        response_data = await rule34_service.get_post_list(tags)
-        if response_data:
-            for item in response_data:
-                file_url = item.get("file_url")
-                if file_url:
-                    result.append(InlineQueryResultPhoto(
-                        id=str(item.get("hash")),
-                        photo_url=file_url,
-                        thumbnail_url=item.get("preview_url"),
-                        caption = f"Source: https://rule34.xxx/index.php?page=post&s=view&id={item.get('id')}"
-                    ))
-                else:
-                    result = []
-    elif service == "danbooru":
-            response_data = await danbooru_service.get_post_list(tags)
+    try:
+        service = inline_query.query.split(" ")[1]
+        tags = inline_query.query.split(f"pic {service}", 1)[-1].strip()
+        result = []
+        if service == "r34":
+            response_data = await rule34_service.get_post_list(tags)
             if response_data:
                 for item in response_data:
                     file_url = item.get("file_url")
                     if file_url:
                         result.append(InlineQueryResultPhoto(
-                            id=str(item.get("md5")),
+                            id=str(item.get("hash")),
                             photo_url=file_url,
-                            thumbnail_url=item.get("preview_file_url"),
-                            caption = f"Source: https://danbooru.donmai.us/posts/{item.get('id')}"
+                            thumbnail_url=item.get("preview_url"),
+                            caption = f"Source: https://rule34.xxx/index.php?page=post&s=view&id={item.get('id')}"
                         ))
-            else:
-                result = []
+                    else:
+                        result = []
+        elif service == "danbooru":
+                response_data = await danbooru_service.get_post_list(tags)
+                if response_data:
+                    for item in response_data:
+                        file_url = item.get("file_url")
+                        if file_url:
+                            result.append(InlineQueryResultPhoto(
+                                id=str(item.get("md5")),
+                                photo_url=file_url,
+                                thumbnail_url=item.get("preview_file_url"),
+                                caption = f"Source: https://danbooru.donmai.us/posts/{item.get('id')}"
+                            ))
+                else:
+                    result = []
+    except:
+        pass
 
     await inline_query.answer(result, is_personal=True)
