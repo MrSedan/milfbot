579b3b7af2442aabfabda3d8eb7187bb3b8f4059
burdukow <brdkv19@gmail.com>
2025-08-10 15:28:38 +0500

    fixed UnboundLocalError error

--- handlers/inline_animations.py
+++ handlers/inline_animations.py
@@ -9,10 +9,11 @@ router = Router()
 
 @router.inline_query(F.query.contains("gif"))
 async def show_user_gifs(inline_query: InlineQuery):
+    result = []
     try:
         service = inline_query.query.split(" ")[1]
         tags = inline_query.query.split(f"gif {service}", 1)[-1].strip()
-        result = []
+
         if service == "r34":
             response_data = await rule34_service.get_post_list(tags)
             if response_data:
--- handlers/inline_images.py
+++ handlers/inline_images.py
@@ -10,10 +10,10 @@ router = Router()
 
 @router.inline_query(F.query.contains("pic"))
 async def show_user_links(inline_query: InlineQuery):
+    result = []
     try:
         service = inline_query.query.split(" ")[1]
         tags = inline_query.query.split(f"pic {service}", 1)[-1].strip()
-        result = []
         if service == "r34":
             response_data = await rule34_service.get_post_list(tags)
             if response_data:
