bc7b821754fecae494d41dbaa1d77b2e896ac33b
burdukow <brdkv19@gmail.com>
2025-08-11 21:56:46 +0500

    feat(services): add pagination and limit parameters to Danbooru and Rule34 API calls

--- services/danbooru_service.py
+++ services/danbooru_service.py
@@ -1,12 +1,13 @@
 from aiohttp import ClientSession, ClientError
 from services.config import settings
 
-async def get_post_list(tags=""):
-    url = f"{settings.danbooru_url}?limit=50&tags={tags}"
+async def get_post_list(tags="", limit=50, page=1):
+    url = f"{settings.danbooru_url}?limit={limit}&page={page}&tags={tags}"
     async with ClientSession() as session:
         try:
             response = await session.get(url)
             response.raise_for_status()
-            return (await response.json())
+            return await response.json()
         except ClientError as e:
             print(f"Error: {e}")
+            return []
--- services/rule34_service.py
+++ services/rule34_service.py
@@ -1,16 +1,18 @@
 from aiohttp import ClientSession
 from services.config import settings
 
-async def get_post_list(tags=""):
+async def get_post_list(tags="", limit=50, page=0):
     async with ClientSession() as session:
-        base_url = f"{settings.rule34_url}?page=dapi&s=post&q=index&json=1&limit=50"
+        base_url = (
+            f"{settings.rule34_url}"
+            f"?page=dapi&s=post&q=index&json=1&limit={limit}&pid={page}"
+        )
         if tags:
-            url = f"{base_url}&tags={tags}"
+            url = f"{base_url}&tags={tags} -ai_generated -ai -ai_assisted"
         else:
             url = base_url
-        response = await session.get(url)
-        if response.status == 200:
-            data = await response.json()
-            return data
-        else:
+
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
             return []
